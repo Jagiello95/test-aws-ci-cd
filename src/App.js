4d6cf9fb9c4712705880ce55b83c6b5e1e7c900c
Andrzej Jagiełło <and.jagiello@gmail.com>
2021-01-30 00:25:32 +0100

    config file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       Hi there again and again and again and again
+       Hi there again and again and again and again and again
         </p>
         <a
           className="App-link"
